2021c7b9e6ef3dc6050b1b285741324056a45a29
te220618 <tejc220618@gmail.com>
2025-05-23 14:11:08 +0900

    コミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class BookController {
     @Autowired
