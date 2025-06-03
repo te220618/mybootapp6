b2e20cf726ce7a1b0bfc21e744b7457959675ea0
te220618 <tejc220618@gmail.com>
2025-06-03 11:51:43 +0900

    課題10コミット

---
+++ src/test/java/jp/te4a/spring/boot/myapp10/mybootapp10/Mybootapp6ApplicationTests.java
@@ -0,0 +1,12 @@
+package jp.te4a.spring.boot.myapp10.mybootapp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookApplication.class)
+class Mybootapp6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
