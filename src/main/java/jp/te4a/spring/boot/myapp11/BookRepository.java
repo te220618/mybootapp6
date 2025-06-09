94f1a42f65557ddae42c118ca8e4070e12d59cb3
te220618 <tejc220618@gmail.com>
2025-06-09 12:21:34 +0900

    課題11コミット

---
+++ src/main/java/jp/te4a/spring/boot/myapp11/BookRepository.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp11;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<BookBean, Integer> {
+    @Query("SELECT x FROM BookBean x ORDER BY x.title")
+
+    List<BookBean> findAllOrderByTitle();
+}
